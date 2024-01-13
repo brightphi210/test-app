2bda1b487416add9d91c2e1f1c0d9bdc60d9b72f
Bright Philip <110538612+brightphi210@users.noreply.github.com>
2024-01-13 03:43:05 -0800

    a

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import "@covalenthq/goldrush-kit/styles.css";
+import {
+  GoldRushProvider,
+  NFTWalletTokenListView,
+  TokenBalancesListView,
+  TokenTransfersListView,
+  AddressActivityListView,
+} from "@covalenthq/goldrush-kit";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+<GoldRushProvider
+          apikey='cqt_rQVQB7HfvQ674VMWWGcRJWJFV3Pf'
+          mode="dark"
+          color="emerald"
+      >
+          <TokenBalancesListView
+              chain_names={[
+                  "eth-mainnet",
+                  "matic-mainnet",
+                  "bsc-mainnet",
+                  "avalanche-mainnet",
+                  "optimism-mainnet",
+              ]}
+              hide_small_balances
+              address="0xfc43f5f9dd45258b3aff31bdbe6561d97e8b71de"
+          />
+          <TokenTransfersListView
+              chain_name="eth-mainnet"
+              address="0xfc43f5f9dd45258b3aff31bdbe6561d97e8b71de"
+              contract_address="0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48"
+          />
+          <AddressActivityListView address="0xfc43f5f9dd45258b3aff31bdbe6561d97e8b71de" />
+          <NFTWalletTokenListView
+              address="0xfc43f5f9dd45258b3aff31bdbe6561d97e8b71de"
+              chain_names={[
+                  "eth-mainnet",
+                  "matic-mainnet",
+                  "bsc-mainnet",
+                  "avalanche-mainnet",
+                  "optimism-mainnet",
+              ]}
+          />
+      </GoldRushProvider>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
